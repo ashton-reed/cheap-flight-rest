2cdfc6caae4dde985f6402deb0cb4b238e9786a8
Ashton Humphrey <ashtonreedhumphrey@gmail.com>
2023-04-29 20:14:33 -0400

    updated pojo

--- src/main/java/com/ashton/reed/cheapflightrest/controller/flight/CheapFlightsController.java
+++ src/main/java/com/ashton/reed/cheapflightrest/controller/flight/CheapFlightsController.java
@@ -1,9 +1,9 @@
-package com.ashton.reed.cheapflightrest.controller;
+package com.ashton.reed.cheapflightrest.controller.flight;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -13,23 +13,20 @@ import java.net.http.HttpResponse;
 @RestController
 public class CheapFlightsController {
 
-    @GetMapping(value = "/flight-data", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public void flights(@RequestBody FlightQu input) throws IOException, InterruptedException {
+    @PostMapping(value = "/flight-query", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseStatus(HttpStatus.OK)
+    public void flights(@RequestBody Root input) throws IOException, InterruptedException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestBody = objectMapper
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(input);
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://skyscanner-api.p.rapidapi.com/v3/flights/live/search/create"))
-                .header("content-type", "application/json")
-                .header("X-RapidAPI-Key", "f97aefd7aemsh71ad17ff2a959d1p14e65djsn82acd5d21dbb")
-                .header("X-RapidAPI-Host", "skyscanner-api.p.rapidapi.com")
-                .method("POST", HttpRequest.BodyPublishers.ofString("" +
-                        "{\n    \"query\": {\n        \"market\": \"UK\",\n        \"locale\": \"en-GB\",\n       " +
-                        " \"currency\": \"EUR\",\n        \"queryLegs\": [\n            {\n               " +
-                        " \"originPlaceId\": {\n                    \"iata\": \"LHR\"\n                },\n            " +
-                        "    \"destinationPlaceId\": {\n                    \"iata\": \"DXB\"\n                },\n    " +
-                        "            \"date\": {\n                    \"year\": 2023,\n                    \"month\": 9,\n   " +
-                        "                 \"day\": 20\n                }\n            }\n        ],\n       " +
-                        "\"cabinClass\": \"CABIN_CLASS_ECONOMY\",\n        \"adults\": 2,\n        \"childrenAges\": [\n      " +
-                        "      3,\n            9\n        ]\n    }\n}"))
-                .build();
+        .uri(URI.create("https://skyscanner-api.p.rapidapi.com/v3/flights/live/search/create"))
+        .header("content-type", "application/json")
+        .header("X-RapidAPI-Key", "f97aefd7aemsh71ad17ff2a959d1p14e65djsn82acd5d21dbb")
+        .header("X-RapidAPI-Host", "skyscanner-api.p.rapidapi.com")
+        .method("POST", HttpRequest.BodyPublishers.ofString(requestBody))
+        .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
     }
--- src/main/java/com/ashton/reed/cheapflightrest/controller/flight/Query.java
+++ src/main/java/com/ashton/reed/cheapflightrest/controller/flight/Query.java
@@ -1,37 +1,96 @@
 package com.ashton.reed.cheapflightrest.controller.flight;
 
 import java.util.List;
-
-    class Date{
+    class Date {
         public int year;
         public int month;
         public int day;
     }
 
-    class DestinationPlaceId{
+    class DestinationPlaceId {
         public String iata;
     }
 
-    class OriginPlaceId{
+    class OriginPlaceId {
         public String iata;
     }
 
-    public class FlightQuery{
+    public class Query {
+        /**
+         * "Market for which the search is for. e.g. `UK`.
+         * SkyScanner provides a Culture API to get all the markets they support."
+         */
         public String market;
+        /**
+         * "Locale that the results are returned in. e.g. `en-GB`(British English).
+         * SkyScanner provides a Culture API to get all the locales they support."
+         */
         public String locale;
+        /**
+         * "Currency that the search results are returned in. e.g. `GBP`.
+         * SkyScanner provides a Culture API to get all the currencies they support."
+         */
         public String currency;
+        /**
+         * "List of legs to search for."
+         * minItems:1 maxItems: 6
+         */
         public List<QueryLeg> queryLegs;
+        /**
+         * 0:"CABIN_CLASS_ECONOMY"
+         * 1:"CABIN_CLASS_PREMIUM_ECONOMY"
+         * 2:"CABIN_CLASS_BUSINESS"
+         * 3:"CABIN_CLASS_FIRST"
+         */
         public String cabinClass;
+        /**
+         * Number of adults. Should be greater than or equal to 1.
+         */
         public int adults;
+        /**
+         * minItems: 0 maxItems: 8
+         */
         public List<Integer> childrenAges;
     }
-
-    class QueryLeg{
+/**
+ * Note: Want to add a return flight add another date to the list
+ * Your originPlaceId should match destinationPlaceId for your return flight
+ *  "queryLegs": [
+ *             {
+ *                 "originPlaceId": {
+ *                     "iata": "LHR"
+ *                 },
+ *                 "destinationPlaceId": {
+ *                     "iata": "DXB"
+ *                 },
+ *                 "date": {
+ *                     "year": 2023,
+ *                     "month": 9,
+ *                     "day": 20
+ *                 }
+ *             },
+ *             {
+ *                 "originPlaceId": {
+ *                     "iata": "DXB"
+ *                 },
+ *                 "destinationPlaceId": {
+ *                     "iata": "LHR"
+ *                 },
+ *                 "date": {
+ *                     "year": 2023,
+ *                     "month": 10,
+ *                     "day": 15
+ *                 }
+ *             }
+ *         ]
+ *
+ */
+class QueryLeg {
         public OriginPlaceId originPlaceId;
         public DestinationPlaceId destinationPlaceId;
         public Date date;
     }
 
     class Root {
-        public FlightQuery query;
+        public Query query;
     }
