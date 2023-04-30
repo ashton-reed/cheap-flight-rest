045c4be1433c031c6cad2966b4dac2768859f762
Ashton Humphrey <ashtonreedhumphrey@gmail.com>
2023-04-30 01:28:40 -0400

    stuff are breaking

--- src/main/java/com/ashton/reed/cheapflightrest/controller/flights/CheapFlightsController.java
+++ src/main/java/com/ashton/reed/cheapflightrest/controller/flights/CheapFlightsController.java
@@ -2,16 +2,20 @@ package com.ashton.reed.cheapflightrest.controller.flights;
 
 import com.ashton.reed.cheapflightrest.models.Root;
 import com.ashton.reed.cheapflightrest.service.CheapFlightService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
 public class CheapFlightsController {
     private final CheapFlightService cheapFlightService;
+    private static String RESULT_STATUS_INCOMPLETE = "RESULT_STATUS_INCOMPLETE";
 
     @Autowired
     public CheapFlightsController(CheapFlightService cheapFlightService) {
@@ -22,11 +26,28 @@ public class CheapFlightsController {
     @ResponseStatus(HttpStatus.OK)
     public void flights(@RequestBody Root input) {
         try {
-            var httpResponse = cheapFlightService.getFlightInfo(input);
-            System.out.println(httpResponse.body());
+            ObjectMapper mapper = new ObjectMapper();
+            var httpResponse = cheapFlightService.postFlightInfo(input);
+            JsonNode node = mapper.readTree(httpResponse.body());
+            String status = node.get("status").asText();
+
             if (httpResponse.statusCode() != 200) {
-                System.out.println("aya it worked");
+                throw new ResponseStatusException(HttpStatus.BAD_GATEWAY," There was an error");
             }
+            if (status.equals(RESULT_STATUS_INCOMPLETE)) {
+                log.info(String.format("Current status.... %s",status));
+                var response = cheapFlightService.getFlightInfoPoll();
+                JsonNode pollNode = mapper.readTree(response.body());
+                String pollStatus = pollNode.get("status").asText();
+                while (pollStatus.equals(RESULT_STATUS_INCOMPLETE)) {
+                    log.info(String.format("Current POLL status.... %s",pollStatus));
+                    cheapFlightService.getFlightInfoPoll();
+                    pollNode = mapper.readTree(response.body());
+                    pollStatus = pollNode.get("status").asText();
+                }
+                log.info("STATUS COMPLETE!");
+            }
+
         } catch(Exception e) {
             log.error("Error: When calling getFlightInfo() ",e);
         }
--- src/main/java/com/ashton/reed/cheapflightrest/service/CheapFlightService.java
+++ src/main/java/com/ashton/reed/cheapflightrest/service/CheapFlightService.java
@@ -9,10 +9,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+
 @Service
 public class CheapFlightService {
 
-    public HttpResponse<String> getFlightInfo(Root input) throws IOException, InterruptedException {
+    public HttpResponse<String> postFlightInfo(Root input) throws IOException, InterruptedException {
         ObjectMapper objectMapper = new ObjectMapper();
         String requestBody = objectMapper
                 .writerWithDefaultPrettyPrinter()
@@ -23,6 +24,16 @@ public class CheapFlightService {
                 .header("X-RapidAPI-Key", "f97aefd7aemsh71ad17ff2a959d1p14e65djsn82acd5d21dbb")
                 .header("X-RapidAPI-Host", "skyscanner-api.p.rapidapi.com")
                 .method("POST", HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+        return HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+    }
+    public HttpResponse<String> getFlightInfoPoll() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://skyscanner-api.p.rapidapi.com/v3/flights/live/search/poll/%7BsessionToken%7D"))
+                .header("content-type", "application/json")
+                .header("X-RapidAPI-Key", "f97aefd7aemsh71ad17ff2a959d1p14e65djsn82acd5d21dbb")
+                .header("X-RapidAPI-Host", "skyscanner-api.p.rapidapi.com")
+                .method("POST", HttpRequest.BodyPublishers.noBody())
                 .build();
         return HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
     }
